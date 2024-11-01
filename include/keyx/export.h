9d41bc897403dc2e85f54a8f427ac0ef2cc61afd
cxxzsh <recovxy@gmail.com>
2024-11-01 22:29:50 +0800

    Add export macro

---
+++ include/keyx/export.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2024 cxxzsh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#ifdef _WIN32
+#ifdef KEYX_EXPORTS
+// Building the library
+#define KEYX_API __declspec(dllexport)
+#else
+// Using the library
+#define KEYX_API __declspec(dllimport)
+#endif
+#else
+#ifdef __GNUC__
+#define KEYX_API __attribute__((visibility("default")))
+#else
+#define KEYX_API
+#endif
+#endif
